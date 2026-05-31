796bf3838c19ef353bab24625030c3530c058e7f
Eleonor2020 <reuk20077@mail.ru>
2026-05-31 17:24:08 +0700

    Классы исключений

--- Laba-3-C#/Program.cs
+++ Laba-3-C#/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+namespace MatrixCalculator
+{
+
+    public class MatrixException: Exception
+    {
+        public MatrixException(string message) : base(message) { }
+    }
+
+    public class MatrixDimensionException : MatrixException
+    {
+        public MatrixDimensionException(string message) : base(message) { }
+    }
+
+    public class SingularMatrixException : MatrixException
+    {
+        public SingularMatrixException(string message) : base(message) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матричный калькулятор");
+            Console.ReadKey();
+        }
+    }
+}
