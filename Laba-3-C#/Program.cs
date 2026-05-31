35ee253495c1bf517bf3116da94fc46a5b2226b7
Eleonor2020 <reuk20077@mail.ru>
2026-05-31 20:11:13 +0700

    Метод ToString

--- Laba-3-C#/Program.cs
+++ Laba-3-C#/Program.cs
@@ -74,6 +74,20 @@ namespace MatrixCalculator
                     data[i, j] = other.data[i, j];
         }
 
+        public override string ToString()
+        {
+            string result = $"Матрица {size}x{size}:\n";
+            for(int i = 0; i < size; i++)
+            {
+                for (int j = 0;j < size; j++)
+                {
+                    result += $"{data[i, j],8:F2} ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
     }
     class Program
     {
