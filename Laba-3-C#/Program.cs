f772f264057a20c795fe79ca3cb028af991193f4
Eleonor2020 <reuk20077@mail.ru>
2026-05-31 17:35:20 +0700

    класс SquareMatrix

--- Laba-3-C#/Program.cs
+++ Laba-3-C#/Program.cs
@@ -18,6 +18,21 @@ namespace MatrixCalculator
         public SingularMatrixException(string message) : base(message) { }
     }
 
+
+    public class SquareMatrix
+    {
+        private double[,] data;
+        private int size;
+
+        public int Size => size;
+
+        public double this[int i, int j]
+        {
+            get { return data[i, j]; }
+            set { data[i, j] = value; }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
